3f0d11433ea6ca23049ffe6baef0e3645ee65839
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-10-02 14:30:23 -0300

    CursorMove hook and Cursor Component

---
+++ src/components/Cursor.jsx
@@ -0,0 +1,22 @@
+import useCursorCircles from "../hooks/cursorCircles";
+
+const Cursor = () => {
+
+    const numberOfCircles = 50
+    const circles = useCursorCircles(numberOfCircles);
+
+    return (
+        <>
+            {circles.map((circle, indexMap) => {return <div key={indexMap} 
+            style={{
+                left: circle.left,
+                top: circle.top,
+                transform: `scale(${circle.scale})`,
+                background: circle.color
+            }}
+            className="cursorCircle"/>})}
+        </>
+    )
+}
+
+export default Cursor
