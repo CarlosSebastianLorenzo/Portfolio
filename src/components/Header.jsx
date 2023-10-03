99ffae3fbf98f941de04f246497e2893cf2a2a5b
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-10-03 14:30:23 -0300

    Header Styles + Nav Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,10 @@ import Nav from "./Nav"
 const Header = () => {
     return (
         <header>
-            <Link to='/'>
-                <img src="../../public/logo.png" alt="Logo Carlos Sebastian Lorenzo" />
-                <h3>Sebastian Lorenzo</h3>
+            <Link to='/' className='logo'>
+                <h1 className='favicon'>S</h1>
+                {/* <img src="../../public/logo.png" alt="Logo Carlos Sebastian Lorenzo" /> */}
+                <h4>Sebastian Lorenzo</h4>
             </Link>
             <Nav/>
         </header>
